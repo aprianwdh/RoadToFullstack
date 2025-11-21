9aa52964fe77c90adac5df7c22337a82af04b31a
aprianwdh <aprianwidhiwibowo@gmail.com>
2025-11-21 20:39:24 +0700

    day 10 - len buat bikin variabel yang bisa dirubah

---
+++ JsDasar/variabel/len.js
@@ -0,0 +1,6 @@
+let nama = "aprian widhi w";
+document.querySelector(".p1").innerHTML = nama;
+nama = "aprian pepek";
+document.querySelector(".p2").innerHTML = nama;
+nama = "aprian sigmws";
+document.querySelector(".p3").innerHTML = nama;
