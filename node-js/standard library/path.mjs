ac0f45a39d74f35c9d18f65804d4731c375a1f70
aprianwdh <aprianwidhiwibowo@gmail.com>
2025-12-10 14:09:26 +0700

    day 27

---
+++ node-js/standard library/path.mjs
@@ -0,0 +1,8 @@
+import path from "path";
+
+// conoth aja lainnya baca di documentasi
+const file = "Users/jmk48/dummy.txt";
+
+console.log(path.dirname(file));
+console.log(path.basename(file));
+console.log(path.extname(file));
