52a3a24e358b10c1ca96de146fca95ee21355205
aprianwdh <aprianwidhiwibowo@gmail.com>
2025-12-10 14:09:19 +0700

    day 27

---
+++ node-js/standard library/os.mjs
@@ -0,0 +1,8 @@
+import os from "os";
+
+// conoth aja lainnya baca di documentasi
+console.log(os.arch());
+console.table(os.cpus());
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
